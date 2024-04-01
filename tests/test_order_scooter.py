7839ae82a19d8050a01fec0bfbea54f5072399a6
Anton TonyL Lysakov <tlysakov@gmail.com>
2024-04-01 21:00:46 +0500

    added assert to scooter order tests

--- tests/test_order_scooter.py
+++ tests/test_order_scooter.py
@@ -1,6 +1,7 @@
 import allure
 
 from locators.home_page_locators import HomePageLocators
+from locators.order_page_locators import OrderPageLocators
 from pages.home_page import HomePage
 from pages.order_page import OrderPage
 from data import TestsData as td
@@ -15,6 +16,7 @@ class TestOrderPage:
         home_page.click_on_element(HomePageLocators.HEADER_ORDER_BUTTON)
         order_page = OrderPage(firefox_driver)
         order_page.scooter_order_flow(ds_1['name'], ds_1['surname'], ds_1['address'], ds_1['phone_number'])
+        assert 'Номер заказа' in firefox_driver.find_element(*OrderPageLocators.PLACED_ORDER_MODAL).text
 
     @allure.title('Успешный заказ самоката с использованием кнопки заказа в теле страницы. Набор данных 2.')
     def test_successful_scooter_order_body_button(self, firefox_driver):
@@ -24,3 +26,5 @@ class TestOrderPage:
         home_page.click_on_element(HomePageLocators.BODY_ORDER_BUTTON)
         order_page = OrderPage(firefox_driver)
         order_page.scooter_order_flow(ds_2['name'], ds_2['surname'], ds_2['address'], ds_2['phone_number'])
+        assert 'Номер заказа' in firefox_driver.find_element(*OrderPageLocators.PLACED_ORDER_MODAL).text
+
